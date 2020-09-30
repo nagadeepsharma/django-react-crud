5df00d4d421f3d799d55f1acb29c78f2004644ca
nagadeepsharma <nagadeep.chandragiri@gmail.com>
2020-09-30 14:34:22 +0530

    'final'

---
+++ frontend/build/precache-manifest.38969641a7d585b9b2f168022348738d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85eb4c4258d9deb32870e8b314e1494b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51e68ed6723ddba1717e",
+    "url": "/static/css/main.5dd1059e.chunk.css"
+  },
+  {
+    "revision": "1f911c779bc16728b3a3",
+    "url": "/static/js/2.2004f6e8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2004f6e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e68ed6723ddba1717e",
+    "url": "/static/js/main.9963d4e9.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
