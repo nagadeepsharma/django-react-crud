4a2c5d9c5f62fc19213302b6a53e5eb543fc3c7b
nagadeepsharma <nagadeep.chandragiri@gmail.com>
2021-06-20 15:26:01 +0530

    saved

--- todo_drf/urls.py
+++ todo_drf/urls.py
@@ -19,6 +19,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', include('api.urls'))
 ]
