f74c6629368ad98187d935fb31682005ece2ed43
nagadeepsharma <nagadeep.chandragiri@gmail.com>
2021-06-20 14:24:56 +0530

    saved

--- todo_drf/settings.py
+++ todo_drf/settings.py
@@ -62,9 +62,7 @@ ROOT_URLCONF = 'todo_drf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'frontend/build')
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,10 +127,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-
-STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'frontend/build/static')
-]
 
 CORS_ALLOW_ALL_ORIGINS=True
 
